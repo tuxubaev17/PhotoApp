3b7f9793e9a3daeafc8723c98118e1498ed0586d
Alikhan <tuxubaev17@gmail.com>
2021-12-21 18:48:59 +0500

    prepare collection view, add plus button and title

--- PhotoApp/Sources/Scenes/AlbumViewController.swift
+++ PhotoApp/Sources/Scenes/AlbumViewController.swift
@@ -9,7 +9,52 @@ import UIKit
 
 class AlbumViewController: UIViewController {
 
-   
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nil, bundle: nil)
+        
+        setupHierarchy()
+        setupLayout()
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let addButton = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .plain, target: self, action: .none)
+        self.navigationItem.leftBarButtonItem = addButton
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.largeTitleDisplayMode = .always
+    }
+    
+    private func setupHierarchy() {
+    
+        view.addSubview(collectionView)
+    }
+    
+    private func setupLayout() {
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15)
+        ])
+    }
+    
+    private func setupView() {
+        title = "Альбомы"
+    }
 
 
 }
